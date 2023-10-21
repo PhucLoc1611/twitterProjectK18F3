335ab00ee1537ecab794469b6033e443c750ec93
locgnguyenphuc@gmail.com <locgnguyenphuc@gmail.com>
2023-10-21 22:09:55 +0700

    project init

---
+++ src/services/database.services.ts
@@ -0,0 +1,31 @@
+import { MongoClient, ServerApiVersion, Db, Collection } from 'mongodb'
+import { config } from 'dotenv'
+import User from '~/models/schemas/User.schema'
+config()
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@tweetprojectk18f3.zxja7xz.mongodb.net/?retryWrites=true&w=majority`
+
+class DatabaseService {
+  private client: MongoClient
+  private db: Db
+  constructor() {
+    this.client = new MongoClient(uri)
+    this.db = this.client.db(process.env.DB_NAME)
+  }
+  async connect() {
+    try {
+      // Send a ping to confirm a successful connection
+      await this.db.command({ ping: 1 })
+      console.log('Pinged your deployment. You successfully connected to MongoDB!')
+    } catch (error) {
+      console.log('Lỗi trong quá trình kết nối mongo', error)
+      throw error
+    }
+  }
+
+  get users(): Collection<User> {
+    return this.db.collection(process.env.DB_USERS_COLLECTION as string)
+  }
+}
+
+const databaseService = new DatabaseService()
+export default databaseService
