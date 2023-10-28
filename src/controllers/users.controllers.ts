942070e5e4bcce94323899bd6d84ac763c801d24
locgnguyenphuc@gmail.com <locgnguyenphuc@gmail.com>
2023-10-28 19:32:20 +0700

    fix login

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -4,14 +4,17 @@ import User from '~/models/schemas/User.schema'
 import databaseService from '~/services/database.services'
 import usersService from '~/services/users.services'
 import { ParamsDictionary } from 'express-serve-static-core'
+import { ErrorWithStatus } from '~/models/Errors'
+import { ObjectId } from 'mongodb'
+import { USERS_MESSAGES } from '~/constants/messages'
 export const loginController = async (req: Request, res: Response) => {
   //nếu nó vào đc đây, tức là nó đã đăng nhập thành công
-  const { user }: any = req
-  const user_id = user._id // ObjectId
+  const user = req.user as User
+  const user_id = user._id as ObjectId
   //server phải tạo ra access_token và refresh_token để đưa cho client
   const result = await usersService.login(user_id.toString()) //login
   return res.json({
-    message: 'Login successfully',
+    message: USERS_MESSAGES.LOGIN_SUCCESS,
     result
   })
 }
@@ -19,7 +22,7 @@ export const loginController = async (req: Request, res: Response) => {
 export const registerController = async (req: Request<ParamsDictionary, any, RegisterReqBody>, res: Response) => {
   const result = await usersService.register(req.body)
   return res.json({
-    message: 'Register successfully',
+    message: USERS_MESSAGES.REGISTER_SUCCESS,
     result
   })
 }
